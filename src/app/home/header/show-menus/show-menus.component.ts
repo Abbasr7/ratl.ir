548c31f8491fca7392bae452e5fdae16163ece16
Abbas Aghayari <Abbas2017@gmail.com>
2022-12-01 10:56:42 +0330

    fixed some issues in responsive menu

--- src/app/home/header/show-menus/show-menus.component.ts
+++ src/app/home/header/show-menus/show-menus.component.ts
@@ -4,6 +4,7 @@ import { IMenu, SuccessHandle } from 'src/app/controlers/interfaces/interfaces';
 import { AuthService } from 'src/app/controlers/services/auth.service';
 import { ServerService } from 'src/app/controlers/services/server.service';
 import { SettingsService } from 'src/app/controlers/services/settings.service';
+import { UserService } from 'src/app/controlers/services/user.service';
 import { Globals } from 'src/app/globals';
 
 @Component({
@@ -15,7 +16,8 @@ export class ShowMenusComponent implements OnInit {
 
   constructor(private server:ServerService,
     private settings:SettingsService,
-    private authService:AuthService) { }
+    private authService:AuthService,
+    private userService: UserService) { }
 
   apiUrl = Globals.publicApi.getMenus
   @Input('loc') loc:string;
@@ -27,6 +29,8 @@ export class ShowMenusComponent implements OnInit {
   logo: string;
   title: string;
   loggedIn: boolean = false;
+  userInfo: any;
+  showSubMenu: boolean = false;
 
   ngOnInit(): void {
     this.getListsAndItems();
@@ -34,6 +38,10 @@ export class ShowMenusComponent implements OnInit {
     if (this.authService.isTokenValid()) {
       this.loggedIn = true
     }
+  }
+
+  ngDoCheck(){
+    this.getUserInfo()
   }
 
   getListsAndItems() {
@@ -68,12 +76,20 @@ export class ShowMenusComponent implements OnInit {
     }
   }
 
+  getUserInfo(){
+    this.userService.userInfo.pipe(
+      take(2)
+    ).subscribe(res => {
+      this.userInfo = res
+    })
+  }
+
   openSubMenu(event:Event) {
     let id = (<HTMLElement>event.target).getAttribute('data-id');
     let subMenu = document.querySelector(`#sub-${id}`)
     subMenu?.classList.toggle('hidden');
+    this.showSubMenu = !this.showSubMenu;
     console.log(event,subMenu);
-    
   }
 
   closeMobileNav() {
@@ -81,6 +97,15 @@ export class ShowMenusComponent implements OnInit {
 
     if (menu.length) {
       menu[0].classList.toggle('hidden');
+    }
+  }
+
+  isExternalLink(url:string) {
+    url = url.toLocaleLowerCase();
+    if (url.includes("https:") || url.includes("http:")) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
